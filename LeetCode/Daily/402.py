4c06c3704d52bfdbaef27b8d09e8c4df6f5b9532
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2024-04-11 21:01:40 -0400

    lc daily

---
+++ LeetCode/Daily/402.py
@@ -0,0 +1,41 @@
+# Thu, APril 11, 2024
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if k >= len(num):
+            return '0'
+
+        sk = []
+
+        for digit in num:
+            while sk and k > 0 and sk[-1] > digit:
+                sk.pop()
+                k -= 1
+
+            sk.append(digit)
+
+        if k > 0:
+            sk = sk[:-k]
+
+        result = ''.join(sk).lstrip('0')
+
+        print(f'result is {result}')
+
+        if result:
+            return result
+
+
+        return '0'
+
+s = Solution()
+
+num = "1432219"
+k = 3
+
+num = "10200"
+k = 1
+
+num = "10"
+k = 2
+
+print(s.removeKdigits(num, k))
